a42ff8709c692adcdb531a94aa36918bdfa2d63b
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-04-29 21:44:41 +0100

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Button } from 'react-mdl';
-import { Link } from "react-scroll";
 import StickyNavbar from './Components/StickyNavbar';
 import IntroPage from './Pages/IntroPage';
 import PageOne from './Pages/PageOne';
 import PageTwo from './Pages/PageTwo';
 import PageThree from './Pages/PageThree';
 import PageFour from './Pages/PageFour';
+import { useSpring, animated } from 'react-spring';
+import { Link } from "react-scroll";
+import Button from 'react-bootstrap/Button'
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <IntroPage />
+            <div className="intro-bottom">
+                <Link
+                    activeClass="active"
+                    to="page-1"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                >
+                    <Button
+                        className="entry-button"
+                        color="primary"
+                    >
+                        <h3
+                            className="entry-button-text"
+                        >
+                            View my work...
+                        </h3>
+                </Button>{' '}
+                </Link>
+            </div>
             <StickyNavbar />
             <PageOne />
             <PageTwo />
--- src/Pages/IntroPage.js
+++ src/Pages/IntroPage.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-scroll";
-import { Button } from 'react-mdl';
+//import { Button } from 'react-mdl';
+import Button from 'react-bootstrap/Button'
+import { useSpring, animated } from 'react-spring';
 
 const IntroPage = () => {
+    const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+
     return (
         <div
             className="intro-page"
-            id="intro-page">
-            <h1 className="nassim-introduction">
-                Hello I'm Nassim, welcome to my Developer Portfolio
-                </h1>
-            <Link
-                activeClass="active"
-                to="page-1"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
+            id="intro-page"
+        >
+            <div
+                className="intro-container"
             >
-                <Button
-                    className="entry-button"
-                    color="primary">
-                    View my work...
+                <animated.h1
+                    className="name-heading"
+                    style={fade}
+                >
+                    Nassim Chenouf
+                </animated.h1>
+                <animated.p
+                    className="profile-paragraph"
+                    style={fade}
+                >
+                    Welcome to my software developer portfolio
+                </animated.p>
+                <div
+                    className="linkedin-and-git"
+                >
+                    <Button
+                        className="linkedin-button"
+                        href="https://www.linkedin.com/in/nassim-chenouf-2b7845135/"
+                        target="_blank"
+                    >
+                        Linkedin
                     </Button>{' '}
-            </Link>
+                    <Button
+                        className="github-button"
+                        href="https://github.com/Nassim55"
+                        target="_blank"
+                        color="primary"
+                    >
+                        GitHub
+                    </Button>{' '}
+                </div>
+            </div>
         </div>
     );
 };
