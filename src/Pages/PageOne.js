56b183647e146460df8ff16a2f5a4972b742d46b
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-05-09 02:16:43 +0100

    09/05/2020 Second Commit

--- src/Pages/PageOne.js
+++ src/Pages/PageOne.js
@@ -8,24 +8,26 @@ const PageOne = () => {
         <div className="page-1" id="page-1">
             <div className="page-1-top-div"></div>
             <div className="container-page-1">
-                <div className="technologies-title">
-                    Languages I've worked with
-                </div>
-                <SimpleSwiper />
+
+                <SimpleSwiper className="simple-swiper"/>
                 <div className="top-container-page-1">
+
                     <div className="top-left-container-page-1">
-                        "I'm a Graduate Aerospace Engineering student from the University
-                        of Leeds. Passionate about building interesting new software.
-                        Main focuses so far have been on front-end developemnt. Interested on
-                        working on machine learning and AI developemnt projects.
-                        CHeck out my projects on GitHub and Contact me (link) "
-                    </div>
-                    <div className="top-right-container-page-1">
                         <img
                             className="my-photo"
                             src={myImage}
                             alt="me"
                         />
+                    </div>
+                    <div className="top-right-container-page-1">
+                        <p>
+                            <span className="quotation-marks">"&nbsp;&nbsp;&nbsp;</span>
+                            
+                            I'm a Graduate Aerospace Engineering student from the University
+                            of Leeds. Passionate about building interesting new software.
+                            
+                            <span className="quotation-marks">&nbsp;&nbsp;&nbsp;"</span>
+                        </p>
                     </div>
                 </div>
             </div>
