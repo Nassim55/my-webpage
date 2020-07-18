4e61c4aa3cc81c60ef58e6c48c48c58f26380cdc
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-07-18 14:06:00 +0100

    Changes to project cards

--- src/Pages/Page3.js
+++ src/Pages/Page3.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ProjectCard from '../Components/PageTwoComponents/ProjectCard';
 
 import marsroverImage from '../Images/marsrover.jpg'; 
+import uavImage from '../Images/uav.jpg';
 import honeycombImage from '../Images/honeycombs.jpg';
 import neuralnetworkImage from '../Images/neuralnetwork.svg';
 
@@ -29,9 +30,16 @@ const Page3 = () => {
             imageSrc = {marsroverImage}
             />
             <ProjectCard
+            projectClassName={'project-card-page-2'}
+            projectTitle={'UNMANNED AERIAL VEHICLE'}
+            projectLanguages={'Python'}
+            rotationDirection = {'180deg'}
+            imageSrc = {uavImage}
+            />
+            <ProjectCard
             className="project-card-page-2"
             projectClassName={'project-card-page-2'}
-            projectTitle={'HYPERSONIC WING DESIGN'}
+            projectTitle={'HYPERSONIC THERMAL MANAGEMENT'}
             projectLanguages={'Python, SolidWorks, ANSYS Fluent'}
             projectHref = {'https://goofy-perlman-2c3af1.netlify.app/'}
             rotationDirection = {'180deg'}
