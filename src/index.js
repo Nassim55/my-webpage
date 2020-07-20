9475b382d32ac5a6267324025b283aebf8c45a5c
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-07-20 21:18:52 +0100

    Testing responsive design changes for non-mobile

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
-import './App.css';
 import './style/css/style.css';
 import './style/css/GenericButton.css'
 
