7c1a84cd44195eba1135d0c1f379990b67a19ab1
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-08-04 00:35:01 +0100

    Added scrollbar to cards

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -11,17 +11,20 @@ const Contact = () => {
                 className="social-icons"
                 url="mailto:nassim.chenouf@outlook.com"
                 fgColor="#ffffff"
-                bgColor="#F24E4E" />
+                bgColor="#F24E4E"
+                style={{ height: 35, width: 35 }}/>
                 <SocialIcon
                 className="social-icons"
                 url="https://www.linkedin.com/in/nassim-chenouf-2b7845135/"
                 fgColor="#ffffff"
-                bgColor="#F24E4E" />
+                bgColor="#F24E4E"
+                style={{ height: 35, width: 35 }}/>
                 <SocialIcon
                 className="social-icons"
                 url="https://github.com/Nassim55"
                 fgColor="#ffffff"
-                bgColor="#F24E4E" />
+                bgColor="#F24E4E"
+                style={{ height: 35, width: 35 }}/>
             </div>
         </div>
     );
