2dba0ffcbafb18887f2429d68b8d355b969190b1
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-08-03 23:06:02 +0100

    Added content to cards

--- src/Components/MoreInfoComponents/AerospaceSiteMoreInfo.js
+++ src/Components/MoreInfoComponents/AerospaceSiteMoreInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const paragraph1 = `Currently in the conceptual design and market research development phase, this project aims to be 
     an interactive, highly visual problem solving based site aimed at aerospace engineering undergraduate and postgraduate
     students. The main focus of this site will be to build an understanding of key aerospace engineering concepts in an
-    intuitive way using an interactive problem solving .`;
+    intuitive way using an interactive problem solving approach.`;
 
 const paragraph2 = `The current planned technologies that will be used to develop this project are React on the front-end
     and Python/Django on the back-end server. Additionally, by using React on the front-end, this opens up the future 
