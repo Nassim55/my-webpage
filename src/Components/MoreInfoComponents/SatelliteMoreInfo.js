9ed5fcae6251bf5a887421b94dcc5b70c987cb2d
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-08-05 21:11:38 +0100

    testing flipped title heights ipad

---
+++ src/Components/MoreInfoComponents/SatelliteMoreInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SatelliteMoreInfo = () => {
+    return (
+        <div className="project-information-container">
+            
+        </div>
+    );
+};
+
+export default SatelliteMoreInfo;
