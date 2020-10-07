340eab668564482963f50b01fbeada98a6b616af
Nassim55 <nassim.chenouf@outlook.com>
2020-10-08 00:55:11 +0100

    Added text for neural etwork

--- src/Components/MoreInfoComponents/SentimentAnalysisMoreInfo.js
+++ src/Components/MoreInfoComponents/SentimentAnalysisMoreInfo.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const paragraph1 = `Currently a work in progress, this project is focused on developing an artificial 
-    neural network to determine sentiment in text.`;
+    neural network to determine sentiment in text. Sentiment analysis has a number of applications that companies
+    can leverage, one such application is the ability to easily route customer emails for high volume companies.
+    If the sentiment in a customer email is negative, the email can  be routed to a highly trained advisor that
+    deals with customer retention and complaints. Neutral and positive emails would be routed to more general staff.`;
 
 const SentimentAnalysisMoreInfo = () => {
     return (
