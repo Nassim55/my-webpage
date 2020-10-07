5eb5d126ecb4381c5d38b6f5a79c157854e8e59b
Nassim55 <nassim.chenouf@outlook.com>
2020-10-08 00:46:29 +0100

    Updated spelling error

--- src/Components/MoreInfoComponents/SentimentAnalysisMoreInfo.js
+++ src/Components/MoreInfoComponents/SentimentAnalysisMoreInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const paragraph1 = `Currently a work in progress, this project project is focused on developing an artificial 
+const paragraph1 = `Currently a work in progress, this project is focused on developing an artificial 
     neural network to determine sentiment in text.`;
 
 const SentimentAnalysisMoreInfo = () => {
